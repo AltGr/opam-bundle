f3bed385eea0bc49c1b9685e591b9dcf45b82193
Louis Gesbert <louis.gesbert@ocamlpro.com>
2019-03-26 18:25:58 +0100

    Make the install script a bit more user-friendly

--- shell/compile.sh
+++ shell/compile.sh
@@ -2,16 +2,49 @@
 
 . "$(dirname "$0")/common.sh"
 
-if [ $# -eq 0 ]; then
-    DESTDIR=
-elif [ $# -eq 1 ] && [ "X${1#-}" = "X$1" ]; then
-    DESTDIR=$1
-else
-   echo "Usage: $0 PREFIX"
-   echo "  Bootstraps and compiles %{install_packages}% within $OPAMROOT,"
-   echo "  then installs wrappers to the given prefix"
-   exit 2
+usage() {
+   echo "Usage: $0 [OPTIONS] PREFIX"
+   echo "  Bootstraps and compiles %{install_packages}% within $DIR,"
+   echo "  then installs wrappers to the given PREFIX if specified."
+   echo
+   echo "Options:"
+   echo "  -h --help		show this help"
+   echo "  -l --list		list the opam packages included"
+   echo "  -y --yes		don't stop for confirmation"
+   exit $1
+}
+
+DESTDIR=
+YES=
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -h|--help) usage 0;;
+    -l|--list) echo "%{install_packages}%"; exit 0;;
+    -y|--yes) YES=1;;
+    -*) usage 2;;
+    *)
+      if [ -z "$DESTDIR" ]; then DESTDIR=$1
+      else usage 2; fi
+  esac
+  shift
+done
+
+if [ "X${DIR#/tmp}" != "X$DIR" ] && [ -z "$YES" ]; then
+  echo "ERROR: you are going to install into /tmp. Everything is likely to get wiped"
+  echo "       on reboot."
+  echo "       Move to a more permanent location, or use '--yes' to force."
+  exit 1
 fi
+
+if [ -z "$DESTDIR" ]; then
+  echo "This bundle will compile the application to $DIR, WITHOUT installing"
+  echo "wrappers anywhere else."
+else
+  echo "This bundle will compile the application to $DIR, and put wrappers into"
+  echo "$DESTDIR/bin. You will need to retain $DIR for the wrappers to work."
+fi
+
+if [ -z "$YES" ]; then printf "\nPress enter to continue... "; read _; fi
 
 if [ ! -e has_depexts ]; then "$DIR/configure.sh"; fi
 
@@ -28,6 +61,7 @@ if [ -z "$DESTDIR" ]; then
     echo "All compiled within $DIR. To use the compiled packages:"
     echo
     echo "  - either re-run $0 with a PREFIX argument to install command wrappers"
+    echo "    (it won't recompile everything)"
     echo
     echo '  - or run the following to update the environment in the current shell, so that'
     echo '    they are in your PATH:'
